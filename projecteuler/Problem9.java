87b2ebd34af531738fc6a708b92040244b4399dc
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-09-18 12:17:29 +0530

    Problem 9 of Project Euler

---
+++ projecteuler/Problem9.java
@@ -0,0 +1,25 @@
+/**
+ * Special Pythagorean triplet
+ * Problem 9
+ * 
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^2 + b^2 = c^2
+ * 
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * 
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
+class Problem9 {
+	final static int SUM = 1000;
+
+	public static void main(String[] args) {
+		for (int a = 1; a <= SUM/3; a++) 
+			for (int b = 1; b <= SUM/2; b++) { 
+				int c = SUM - b - a; 
+				if (((a < b) && (b < c)) && (a*a + b*b == c*c) && (a+b+c == SUM))
+					System.out.println(a*b*c);
+			}
+	}
+}
